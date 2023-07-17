d8e46e9bc2c6f6d3a1075cb07281a2aac9c7af14
0num4 <r.oonuma@matsuri-tech.com>
2023-07-18 07:23:32 +0900

    feat: agari.ts

--- mahjong_ts/hand_calculating/hand.ts
+++ mahjong_ts/hand_calculating/hand.ts
@@ -1,7 +1,7 @@
 import { HandConfig } from "./hand_config";
 
 class HandCalculator {
-  config = null;
+  config: HandConfig | null = null;
 
   ERR_NO_WINNING_TILE = "winning_tile_not_in_hand";
   ERR_OPEN_HAND_RIICHI = "open_hand_riichi_not_allowed";
@@ -25,28 +25,57 @@ class HandCalculator {
   ERR_RENHOU_AS_DEALER = "renhou_as_dealer_not_allowed";
   ERR_RENHOU_WITH_TSUMO = "renhou_with_tsumo_not_allowed";
   ERR_RENHOU_WITH_MELD = "renhou_with_meld_not_allowed";
+  hand_divider: any;
 
   constructor() {
     this.config = new HandConfig();
   }
-  // more possible errors, like tenhou and haitei can't be together (so complicated :<)
-  estimate_hand_value(
-    tiles,
-    win_tile,
-    melds,
-    dora_indicators,
-    config,
-    scores_calculator_factory,
-    use_hand_divider_cache
-  ) {
-    if (!melds) {
-      melds = [];
+  estimate_hand_value(tiles: number[], win_tile: number, is_tsumo: boolean, is_riichi: boolean, open_sets: number[][] | null = null, use_hand_divider_cache: boolean = true): any {
+    this.hand_divider = new HandDivider();
+
+    let agari = new Agari();
+    let tiles_34 = TilesConverter.to_34_array(tiles);
+    this.hand_total_tiles = tiles_34.reduce((a, b) => a + b, 0);
+
+    this.config.is_tsumo = is_tsumo;
+    this.config.is_riichi = is_riichi;
+
+    let open_hand = false;
+    let melds;
+
+    if (open_sets) {
+        open_hand = true;
+        melds = open_sets.map(x => new Meld(x));
+    } else {
+        melds = [];
     }
 
-    if (!dora_indicators) {
-      dora_indicators = [];
+    this.config.is_open_hand = open_hand;
+
+    if (this.config.is_nagashi_mangan) {
+        return new HandResponse({error: HandCalculator.ERR_NAGASHI_MANGAN});
     }
 
-    this.config = config || new HandConfig();
+    if (this.config.is_nagashi_mangan) {
+      return new HandResponse({error: HandCalculator.ERR_NAGASHI_MANGAN});
   }
+  
+  if (this.config.is_renhou && !this.config.options.has_renhou) {
+      return new HandResponse({error: HandCalculator.ERR_RENHOU_IS_CLOSED});
+  }
+  
+  if (this.config.is_renhou && this.config.options.renhou_as_yakuman) {
+      this.config.yaku.renhou.rename();
+      return new HandResponse({
+          cost: scores_calculator.calculate_scores(13, 0, this.config, true),
+          han: 13,
+          fu: 0,
+          hand_yaku: [this.config.yaku.renhou_yakuman],
+          error: null,
+      });
+  }
+
+    if (!agari.is_agari(tiles_34, all_melds)) {
+        return new HandResponse({error: HandCalculator.ERR_HAND_NOT_WINNING});
+    }
 }
